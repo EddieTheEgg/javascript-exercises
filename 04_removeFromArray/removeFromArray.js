4a26ada53ca8b53b9caa9e4ee570a9f728f93758
Edison Kwok <edditheeggy@gmail.com>
2024-03-22 19:15:21 -0400

    More js practice (array deconstructing, rest parameters,)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
 
+let removeFromArray = function(array, target) {
+    return array.filter(element => element !== target); //Filter passes each element from the array to another function which has no name. But the value passed ino the function is 
+    //called element and is compared to target. If the element is equal to the target, the filter() method will remove the value.
 };
+
+let newList = ["hi", "bye", "yogatti", "bruh"];
+
+console.log(removeFromArray(newList, "bye"));
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(min,max) {
+
+    if (min > max){ //checking parameters
+        let temp = min;
+        min = max;
+        max = temp;
+        //Another option would be [min, max] = [max, min] which is called array deconstructing. The parameters passed in can be
+  
+    let sum = 0;  //actually getting the sum loop
+    for (let i = min; i<max+1; i++){
+        sum+=i;
+    }
+    return sum;
 
 };
 
+console.log(sumAll(4,2));
+
 // Do not edit below this line
 module.exports = sumAll;
+}
