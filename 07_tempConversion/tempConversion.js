f59be2c939fa34a1b58f20a579fb40d807295b8c
Edison Kwok <edditheeggy@gmail.com>
2024-03-23 22:30:55 -0400

    temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenTemp) {
+
+  return Math.round((fahrenTemp-32)*(5/9)*10) / 10 + " Celsius";
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+
+  return Math.round((celsiusTemp * (9/5) + 32) * 10) / 10 + " Fahrenheit";
 };
+
+console.log(convertToCelsius(48));
+console.log(convertToFahrenheit(12));
+
 
 // Do not edit below this line
 module.exports = {
