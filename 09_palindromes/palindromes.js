892dbd2705a73650809ca31492cdee9ed21a4f38
Edison Kwok <edditheeggy@gmail.com>
2024-05-05 13:40:39 -0400

    Two new assigments completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const processedString = string.toLowerCase().replace(/[\W_]/g, ''); //replace all weird characters with a ''
+    const reversedString = processedString.split("").reverse().join("");
 
+    return processedString === reversedString;
 };
+
+console.log(palindromes("racecar"));
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+
+
+    const array = [1,1];
+    //Checking if the parameter is ok
+
+    if (count == 1 || count == 2 ) {
+        return array[count-1];
+    }
+    else if (count >2){
+        for (let i = 2; i < count; i++){
+            array[i] = array[i-1] + array[i-2];
+        }
+    }
+    else{
+        return `the input is kinda cringe, try reinputting an integer please. Your input was "${count}"`;
+    }
+
+    return array[count-1];
 
 };
 
+
+console.log(fibonacci("fifteen"));
 // Do not edit below this line
 module.exports = fibonacci;
